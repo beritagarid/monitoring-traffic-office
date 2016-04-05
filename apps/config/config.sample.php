501645edcce36174da36eab17212a33d6f1b8433
BeritagarID <beritagarid@users.noreply.github.com>
2016-04-05 09:13:52 +0700

    Update config.sample.php

--- apps/config/config.sample.php
+++ apps/config/config.sample.php
@@ -1,6 +1,12 @@
 <?php
-return array(
-    'host' => 'localhost',
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+$config = array(
+    'host' => '127.0.0.1',
     'username' => 'username',
-    'password' => 'password'
+    'password' => 'password',
+    'logo'     => 'https://beritagar.id/images/logo-beritagar.svg',
+    'template' => 'desktop',
+    'server'   => 'Beritagar'
 );
