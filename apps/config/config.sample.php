427968859639c5d58ed640a794bf976685759985
Dewangga Bachrul Alam <dewangga@beritagar.id>
2016-04-11 16:06:32 +0700

    fixing config

--- apps/config/config.sample.php
+++ apps/config/config.sample.php
@@ -1,6 +1,12 @@
 <?php
-return array(
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+$config = array(
     'host' => 'localhost',
     'username' => 'username',
-    'password' => 'password'
+    'password' => 'password',
+    'logo'     => 'https://beritagar.id/images/logo-beritagar.svg',
+    'template' => 'desktop',
+    'server'   => 'Beritagar'
 );
