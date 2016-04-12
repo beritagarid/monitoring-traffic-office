b52b96e9b3bfcccf4bcd1f31d2dba87d9003299e
Eka Candra Setyobudi <eka@beritagar.id>
2016-04-12 16:20:27 +0700

    fixing invalid sample config

--- apps/config/config.sample.php
+++ apps/config/config.sample.php
@@ -3,11 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
 $config = array(
-<<<<<<< HEAD
     'host' => '127.0.0.1',
-=======
-    'host' => 'localhost',
->>>>>>> 427968859639c5d58ed640a794bf976685759985
     'username' => 'username',
     'password' => 'password',
     'logo'     => 'https://beritagar.id/images/logo-beritagar.svg',
